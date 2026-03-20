5f82e76c1ff285966bc41788b41b77c994caec2e
freeliotech <freeliotechlogin@gmail.com>
2026-03-20 19:57:53 +0530

    .env file added

--- src/config/Api.jsx
+++ src/config/Api.jsx
@@ -1,4 +1,8 @@
 // src/config/api.js
 import axios from "axios";
 
-export const ApiContext = "https://backend-production-7a212.up.railway.app/api";
+// export const ApiContext = "https://backend-production-7a212.up.railway.app/api";
+
+// import axios from "axios";
+
+export const ApiContext = import.meta.env.VITE_API_URL;
